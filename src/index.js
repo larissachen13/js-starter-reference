a3a2c736e79396e4bcaae6afb04da6d22033d208
Larissa Chen <larissa.j.chen.18@dartmouth.edu>
2016-07-14 19:54:29 -0400

    finished site

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 // es6 import style
-import $ from 'jquery';
-
 import './style.scss';
+import debounce from 'lodash.debounce';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import youtubeSearch from './youtube-api';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
-// this is a generator function.
-// note the *
-function *simpleCounter() {
-  let count = 0;
-  while (true) {
-    yield count++;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 300);
+  }
+  search(text) {
+    youtubeSearch(text).then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar id="searchbar" onSearchChange={text => this.search(text)} />
+        <div className="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
   }
 }
 
-// instantiate generator
-const counter = simpleCounter();
-
-setInterval(
-  // arrow notation for anonymous function
-  () => { $('#main').html(`You've been on this page for ${counter.next().value} seconds.`); }
-  , 1000
-);
+ReactDOM.render(<App />, document.getElementById('main'));
